00930806fdc1eee52df4f7ebde510e476d34088d
Dusan Trtica <dusantrtica@M-CJK94RXMVC.local>
2025-03-24 19:11:09 +0100

    added tests for library

--- point2d/test/Spec.hs
+++ point2d/test/Spec.hs
@@ -1,2 +1,8 @@
-main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+import Test.HUnit
+import Lib
+
+-- test for quadrantPoint
+test1 = TestCase (assertEqual "test1" 0 (quadrantPoint (0, 0)))
+
+testsRequested = TestList [TestLabel "test1" test1]
+main = runTestTT testsRequested
